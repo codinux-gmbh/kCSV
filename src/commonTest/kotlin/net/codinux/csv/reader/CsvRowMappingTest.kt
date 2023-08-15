5aea3448a064fbabef6b2a22bf71972ebc48e1e7
dankito <git@dankito.net>
2023-08-15 15:22:59 +0200

    Added tests for 'OrNull' also for all other data types

--- src/commonTest/kotlin/net/codinux/csv/reader/CsvRowMappingTest.kt
+++ src/commonTest/kotlin/net/codinux/csv/reader/CsvRowMappingTest.kt
@@ -1,50 +1,69 @@
 package net.codinux.csv.reader
 
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
-class CsvRowMappingTest {
+class CsvRowMappingTest : FunSpec({
+
+    NullTestCases.forEach { (name, fieldValue) ->
+
+        test("stringOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getStringOrNull(0).shouldBeNull()
+            }
+        }
+
+        test("booleanOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getBooleanOrNull(0).shouldBeNull()
+            }
+        }
+
+        test("intOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getIntOrNull(0).shouldBeNull()
+            }
+        }
+
+        test("longOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getLongOrNull(0).shouldBeNull()
+            }
+        }
+
+        test("floatOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getFloatOrNull(0).shouldBeNull()
+            }
+        }
+
+        test("doubleOrNull_$name") {
+            CsvReader(fieldValue).forEach { row ->
+                row.getDoubleOrNull(0).shouldBeNull()
+            }
+        }
+
+    }
+
+}) {
+
+    companion object {
+        val NullTestCases = mapOf(
+            "EmptyString" to ",",
+            // https://en.wikipedia.org/wiki/Whitespace_character
+            "BlankString" to " \u00A0 \t \u000C \u1680 \u2000",
+            "NullLowercase" to "null",
+            "NullUppercase" to "NULL",
+            "NullMixedCase" to "nUlL"
+        )
+    }
 
     @Test
     fun string() {
         CsvReader("string").forEach { row ->
             row.getString(0).shouldBe("string")
-        }
-    }
-
-    @Test
-    fun stringOrNull_EmptyString() {
-        CsvReader(",").forEach { row ->
-            row.getStringOrNull(0).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_BlankString() {
-        CsvReader(" \u00A0 \t \u000C \u1680 \u2000").forEach { row ->
-            row.getStringOrNull(0).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullLowercase() {
-        CsvReader("null").forEach { row ->
-            row.getStringOrNull(0).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullUppercase() {
-        CsvReader("NULL").forEach { row ->
-            row.getStringOrNull(0).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullMixedCase() {
-        CsvReader("nUlL").forEach { row ->
-            row.getStringOrNull(0).shouldBeNull()
         }
     }
 
--- src/commonTest/kotlin/net/codinux/csv/reader/NamedCsvRowMappingTest.kt
+++ src/commonTest/kotlin/net/codinux/csv/reader/NamedCsvRowMappingTest.kt
@@ -1,10 +1,53 @@
 package net.codinux.csv.reader
 
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
-class NamedCsvRowMappingTest {
+class NamedCsvRowMappingTest : FunSpec({
+
+    CsvRowMappingTest.NullTestCases.mapValues { (_, value) -> if (value == ",") "" else value }.forEach { (name, fieldValue) ->
+
+        test("stringOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getStringOrNull(Header).shouldBeNull()
+            }
+        }
+
+        test("booleanOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getBooleanOrNull(Header).shouldBeNull()
+            }
+        }
+
+        test("intOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getIntOrNull(Header).shouldBeNull()
+            }
+        }
+
+        test("longOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getLongOrNull(Header).shouldBeNull()
+            }
+        }
+
+        test("floatOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getFloatOrNull(Header).shouldBeNull()
+            }
+        }
+
+        test("doubleOrNull_$name") {
+            NamedCsvReader("${HeaderLine}$fieldValue").forEach { row ->
+                row.getDoubleOrNull(Header).shouldBeNull()
+            }
+        }
+
+    }
+
+})  {
 
     companion object {
         private const val Header = "header"
@@ -15,41 +58,6 @@ class NamedCsvRowMappingTest {
     fun string() {
         NamedCsvReader("${HeaderLine}string").forEach { row ->
             row.getString(Header).shouldBe("string")
-        }
-    }
-
-    @Test
-    fun stringOrNull_EmptyString() {
-        NamedCsvReader(HeaderLine).forEach { row ->
-            row.getStringOrNull(Header).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_BlankString() {
-        NamedCsvReader("$HeaderLine \u00A0 \t \u000C \u1680 \u2000").forEach { row ->
-            row.getStringOrNull(Header).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullLowercase() {
-        NamedCsvReader("${HeaderLine}null").forEach { row ->
-            row.getStringOrNull(Header).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullUppercase() {
-        NamedCsvReader("${HeaderLine}NULL").forEach { row ->
-            row.getStringOrNull(Header).shouldBeNull()
-        }
-    }
-
-    @Test
-    fun stringOrNull_NullMixedCase() {
-        NamedCsvReader("${HeaderLine}nUlL").forEach { row ->
-            row.getStringOrNull(Header).shouldBeNull()
         }
     }
 
