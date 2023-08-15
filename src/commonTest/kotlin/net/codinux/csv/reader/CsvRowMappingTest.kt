b04342a18834e23e94e6ba490ef32b86064d6dee
dankito <git@dankito.net>
2023-08-15 15:54:44 +0200

    Added tests for numeric fields

--- src/commonTest/kotlin/net/codinux/csv/reader/CsvRowMappingTest.kt
+++ src/commonTest/kotlin/net/codinux/csv/reader/CsvRowMappingTest.kt
@@ -1,8 +1,10 @@
 package net.codinux.csv.reader
 
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import kotlin.random.Random
 import kotlin.test.Test
 
 class CsvRowMappingTest : FunSpec({
@@ -47,6 +49,42 @@ class CsvRowMappingTest : FunSpec({
 
     }
 
+    (1..100).forEach { index ->
+
+        test("[$index] Random Int test") {
+            val expected = Random.nextInt()
+
+            CsvReader(expected.toString()).forEach { row ->
+                row.getInt(0).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Long test") {
+            val expected = Random.nextLong()
+
+            CsvReader(expected.toString()).forEach { row ->
+                row.getLong(0).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Float test") {
+            val expected = Random.nextFloat()
+
+            CsvReader(expected.toString()).forEach { row ->
+                row.getFloat(0).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Double test") {
+            val expected = Random.nextDouble()
+
+            CsvReader(expected.toString()).forEach { row ->
+                row.getDouble(0).shouldBe(expected)
+            }
+        }
+
+    }
+
 }) {
 
     companion object {
@@ -64,6 +102,154 @@ class CsvRowMappingTest : FunSpec({
     fun string() {
         CsvReader("string").forEach { row ->
             row.getString(0).shouldBe("string")
+        }
+    }
+
+
+    @Test
+    fun boolean_TrueLowercase() {
+        CsvReader("true").forEach { row ->
+            row.getBoolean(0).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_TrueUppercase() {
+        CsvReader("TRUE").forEach { row ->
+            row.getBoolean(0).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_TrueMixedCase() {
+        CsvReader("tRuE").forEach { row ->
+            row.getBoolean(0).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_FalseLowercase() {
+        CsvReader("false").forEach { row ->
+            row.getBoolean(0).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_FalseUppercase() {
+        CsvReader("FALSE").forEach { row ->
+            row.getBoolean(0).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_FalseMixedCase() {
+        CsvReader("fAlSe").forEach { row ->
+            row.getBoolean(0).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_OtherString() {
+        CsvReader("Truthy").forEach { row ->
+            shouldThrowAny {
+                row.getBoolean(0)
+            }
+        }
+    }
+
+
+    @Test
+    fun int_Max() {
+        CsvReader(Int.MAX_VALUE.toString()).forEach { row ->
+            row.getInt(0).shouldBe(Int.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun int_MaxPlus1() {
+        CsvReader((Int.MAX_VALUE.toLong() + 1).toString()).forEach { row ->
+            shouldThrowAny {
+                row.getInt(0)
+            }
+        }
+    }
+
+    @Test
+    fun int_Min() {
+        CsvReader(Int.MIN_VALUE.toString()).forEach { row ->
+            row.getInt(0).shouldBe(Int.MIN_VALUE)
+        }
+    }
+
+    @Test
+    fun int_MinMinus1() {
+        CsvReader((Int.MIN_VALUE.toLong() - 1).toString()).forEach { row ->
+            shouldThrowAny {
+                row.getInt(0)
+            }
+        }
+    }
+
+
+    @Test
+    fun long_Max() {
+        CsvReader(Long.MAX_VALUE.toString()).forEach { row ->
+            row.getLong(0).shouldBe(Long.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun long_MaxPlus1() {
+        CsvReader("9223372036854775808").forEach { row ->
+            shouldThrowAny {
+                row.getLong(0)
+            }
+        }
+    }
+
+    @Test
+    fun long_Min() {
+        CsvReader(Long.MIN_VALUE.toString()).forEach { row ->
+            row.getLong(0).shouldBe(Long.MIN_VALUE)
+        }
+    }
+
+    @Test
+    fun long_MinMinus1() {
+        CsvReader("-9223372036854775809").forEach { row ->
+            shouldThrowAny {
+                row.getLong(0)
+            }
+        }
+    }
+
+
+    @Test
+    fun float_Max() {
+        CsvReader(Float.MAX_VALUE.toString()).forEach { row ->
+            row.getFloat(0).shouldBe(Float.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun float_Min() {
+        CsvReader(Float.MIN_VALUE.toString()).forEach { row ->
+            row.getFloat(0).shouldBe(Float.MIN_VALUE)
+        }
+    }
+
+
+    @Test
+    fun double_Max() {
+        CsvReader(Double.MAX_VALUE.toString()).forEach { row ->
+            row.getDouble(0).shouldBe(Double.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun double_Min() {
+        CsvReader(Double.MIN_VALUE.toString()).forEach { row ->
+            row.getDouble(0).shouldBe(Double.MIN_VALUE)
         }
     }
 
--- src/commonTest/kotlin/net/codinux/csv/reader/NamedCsvRowMappingTest.kt
+++ src/commonTest/kotlin/net/codinux/csv/reader/NamedCsvRowMappingTest.kt
@@ -1,8 +1,10 @@
 package net.codinux.csv.reader
 
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import kotlin.random.Random
 import kotlin.test.Test
 
 class NamedCsvRowMappingTest : FunSpec({
@@ -47,6 +49,42 @@ class NamedCsvRowMappingTest : FunSpec({
 
     }
 
+    (1..100).forEach { index ->
+
+        test("[$index] Random Int test") {
+            val expected = Random.nextInt()
+
+            NamedCsvReader("$HeaderLine$expected").forEach { row ->
+                row.getInt(Header).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Long test") {
+            val expected = Random.nextLong()
+
+            NamedCsvReader("$HeaderLine$expected").forEach { row ->
+                row.getLong(Header).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Float test") {
+            val expected = Random.nextFloat()
+
+            NamedCsvReader("$HeaderLine$expected").forEach { row ->
+                row.getFloat(Header).shouldBe(expected)
+            }
+        }
+
+        test("[$index] Random Double test") {
+            val expected = Random.nextDouble()
+
+            NamedCsvReader("$HeaderLine$expected").forEach { row ->
+                row.getDouble(Header).shouldBe(expected)
+            }
+        }
+
+    }
+
 })  {
 
     companion object {
@@ -58,6 +96,154 @@ class NamedCsvRowMappingTest : FunSpec({
     fun string() {
         NamedCsvReader("${HeaderLine}string").forEach { row ->
             row.getString(Header).shouldBe("string")
+        }
+    }
+
+
+    @Test
+    fun boolean_TrueLowercase() {
+        NamedCsvReader("${HeaderLine}true").forEach { row ->
+            row.getBoolean(Header).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_TrueUppercase() {
+        NamedCsvReader("${HeaderLine}TRUE").forEach { row ->
+            row.getBoolean(Header).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_TrueMixedCase() {
+        NamedCsvReader("${HeaderLine}tRuE").forEach { row ->
+            row.getBoolean(Header).shouldBe(true)
+        }
+    }
+
+    @Test
+    fun boolean_FalseLowercase() {
+        NamedCsvReader("${HeaderLine}false").forEach { row ->
+            row.getBoolean(Header).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_FalseUppercase() {
+        NamedCsvReader("${HeaderLine}FALSE").forEach { row ->
+            row.getBoolean(Header).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_FalseMixedCase() {
+        NamedCsvReader("${HeaderLine}fAlSe").forEach { row ->
+            row.getBoolean(Header).shouldBe(false)
+        }
+    }
+
+    @Test
+    fun boolean_OtherString() {
+        NamedCsvReader("${HeaderLine}Truthy").forEach { row ->
+            shouldThrowAny {
+                row.getBoolean(Header)
+            }
+        }
+    }
+
+
+    @Test
+    fun int_Max() {
+        NamedCsvReader("$HeaderLine${Int.MAX_VALUE}").forEach { row ->
+            row.getInt(Header).shouldBe(Int.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun int_MaxPlus1() {
+        NamedCsvReader("$HeaderLine${Int.MAX_VALUE.toLong() + 1}").forEach { row ->
+            shouldThrowAny {
+                row.getInt(Header)
+            }
+        }
+    }
+
+    @Test
+    fun int_Min() {
+        NamedCsvReader("$HeaderLine${Int.MIN_VALUE}").forEach { row ->
+            row.getInt(Header).shouldBe(Int.MIN_VALUE)
+        }
+    }
+
+    @Test
+    fun int_MinMinus1() {
+        NamedCsvReader("$HeaderLine${Int.MIN_VALUE.toLong() - 1}").forEach { row ->
+            shouldThrowAny {
+                row.getInt(Header)
+            }
+        }
+    }
+
+
+    @Test
+    fun long_Max() {
+        NamedCsvReader("$HeaderLine${Long.MAX_VALUE}").forEach { row ->
+            row.getLong(Header).shouldBe(Long.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun long_MaxPlus1() {
+        NamedCsvReader("${HeaderLine}9223372036854775808").forEach { row ->
+            shouldThrowAny {
+                row.getLong(Header)
+            }
+        }
+    }
+
+    @Test
+    fun long_Min() {
+        NamedCsvReader("$HeaderLine${Long.MIN_VALUE}").forEach { row ->
+            row.getLong(Header).shouldBe(Long.MIN_VALUE)
+        }
+    }
+
+    @Test
+    fun long_MinMinus1() {
+        NamedCsvReader("${HeaderLine}-9223372036854775809").forEach { row ->
+            shouldThrowAny {
+                row.getLong(Header)
+            }
+        }
+    }
+
+
+    @Test
+    fun float_Max() {
+        NamedCsvReader("$HeaderLine${Float.MAX_VALUE}").forEach { row ->
+            row.getFloat(Header).shouldBe(Float.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun float_Min() {
+        NamedCsvReader("$HeaderLine${Float.MIN_VALUE}").forEach { row ->
+            row.getFloat(Header).shouldBe(Float.MIN_VALUE)
+        }
+    }
+
+
+    @Test
+    fun double_Max() {
+        NamedCsvReader("$HeaderLine${Double.MAX_VALUE}").forEach { row ->
+            row.getDouble(Header).shouldBe(Double.MAX_VALUE)
+        }
+    }
+
+    @Test
+    fun double_Min() {
+        NamedCsvReader("$HeaderLine${Double.MIN_VALUE}").forEach { row ->
+            row.getDouble(Header).shouldBe(Double.MIN_VALUE)
         }
     }
 
